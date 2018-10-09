2e67ea390893ecb36a712c7e4e637de0a28500df
Sean <sdgerbich@gmail.com>
2018-10-09 14:18:09 -0400

    connection stuff

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,20 @@
 var mysql = require("mysql");
 
 // Set up our connection information
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  // DB is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 
-var connection2 =
-  {
-    production: {
-      username: "ojr7hbmab1wa8pa2",
-      password: "rm6yqc4v5tiuvpgh",
-      database: "lxgb2h1fmsdrhcsx",
-      host: "wftuqljwesiffol6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-      dialect: "mysql"
-    }
-  }
+  var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
+
 // Connect to the database
 connection.connect(function (err) {
   if (err) {
@@ -29,4 +25,4 @@ connection.connect(function (err) {
 });
 
 // Export connection
-module.exports = connection2;
+module.exports = connection;
